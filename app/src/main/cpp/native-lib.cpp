c6e0b8217e981373760ba5bb2ae24e8e2ceaa3ff
oilpal <jihoonl@gmail.com>
2021-09-19 23:19:06 +0900

    remove hello_world native code

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,14 +5,6 @@
 
 using namespace cv;
 
-//extern "C" JNIEXPORT jstring JNICALL
-//Java_com_example_myapplication_MainActivity_stringFromJNI(
-//        JNIEnv* env,
-//        jobject /* this */) {
-//    std::string hello = "Hello from C++";
-//    return env->NewStringUTF(hello.c_str());
-//}
-
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_example_opencvex_MainActivity_ConvertRGBtoGray(JNIEnv *env, jobject thiz,
