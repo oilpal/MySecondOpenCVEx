ad0bc7bf67256167cc2c973419f08901984916b8
oilpal <jihoonl@gmail.com>
2021-09-19 22:48:47 +0900

    first commit

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,27 @@
+#include <jni.h>
+#include <string>
+
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+//extern "C" JNIEXPORT jstring JNICALL
+//Java_com_example_myapplication_MainActivity_stringFromJNI(
+//        JNIEnv* env,
+//        jobject /* this */) {
+//    std::string hello = "Hello from C++";
+//    return env->NewStringUTF(hello.c_str());
+//}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_opencvex_MainActivity_ConvertRGBtoGray(JNIEnv *env, jobject thiz,
+                                                        jlong mat_addr_input,
+                                                        jlong mat_addr_result) {
+    // TODO: implement ConvertRGBtoGray()
+
+    Mat &matInput = *(Mat *)mat_addr_input;
+    Mat &matResult = *(Mat *)mat_addr_result;
+
+    cvtColor(matInput, matResult, COLOR_RGBA2GRAY);
+}
